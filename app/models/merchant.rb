b293bdc60f28da4ce729db5e1a72e825bc9f6ffd
William Lampke <gravitybangers@gmail.com>
2023-01-13 17:45:23 -0500

    add discounts and realtionships

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,9 +1,11 @@
 class Merchant < ApplicationRecord
   has_many :items
+  has_many :discounts
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
+  
 
   def self.top5(id)
     Customer
